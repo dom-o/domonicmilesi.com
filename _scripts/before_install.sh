19bce3457418037ded5d0c1d83577eabdc783c9a
Domonic Milesi <dom-o@users.noreply.github.com>
2021-04-26 17:18:29 -0400

    Add spotify-recs-controller link to main page.

--- _scripts/before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-openssl aes-256-cbc -K $encrypted_189e52c2c347_key -iv $encrypted_189e52c2c347_iv -in deploy_key.enc -out deploy_key -d
-rm deploy_key.enc # Don't need it anymore
-chmod 600 deploy_key
-mv deploy_key ~/.ssh/id_rsa
--- _scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-#install node dependencies
-npm ci --production
-#build the site with metalsmith
-npx metalsmith
